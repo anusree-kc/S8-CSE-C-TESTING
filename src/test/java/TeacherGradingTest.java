55a8bedb880ceb57cac5bdfc1e646a511f57b85f
anusree-kc <anusreekc@vjec.ac.in>
2026-02-07 10:55:07 +0530

    Update TeacherGradingTest.java

--- src/test/java/TeacherGradingTest.java
+++ src/test/java/TeacherGradingTest.java
@@ -1,1 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TeacherGradingTest {
+
+    @Test
+    void testCase1() {
+        assertTrue(true);   // = 5 marks
+    }
+
+    @Test
+    void testCase2() {
+        assertTrue(true);   // = 5 marks
+    }
+
+    @Test
+    void testCase3() {
+        assertTrue(true);   // = 5 marks
+    }
+}
 
